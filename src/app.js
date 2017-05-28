de932abd315590ed0e019976a77431df5b5bde89
Williams YEO <wyeo@student.42.fr>
2017-05-28 17:39:21 +0200

    without websocket server

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/index'
-import Game from './components/Game'
+import Game from './components/GameContainer'
 
 window.store = store
 
